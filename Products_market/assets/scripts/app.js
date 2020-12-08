8e7b867a7f20bbe98e479d0c03bdffa21aacdfe4
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2020-12-09 00:45:38 +0200

    using constructor && Super to re use the code

--- Products_market/assets/scripts/app.js
+++ Products_market/assets/scripts/app.js
@@ -7,11 +7,50 @@ class Product {
   }
 }
 
-class ShoppingCart {
+class ElementAttributes {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      // Here is refer to each subClass render
+      this.render();
+    }
+  }
+
+  // Useless method to show that the this.render is refer for the Component class
+  render() {}
+  createRootElement(tag, cssClasses, attributes) {
+    // createRootElement(tag, cssClasses) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+        // console.log(attr.name + "  " + attr.value);
+      }
+    }
+
+    // use Super() Help to pass the hookId
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
   items = [];
   set cartItems(value) {
     this.items = value;
-    this.totalOutput.innerHTML = ` <h2> Total : \$${this.totalAmmount.toFixed(2)}</h2>`;
+    this.totalOutput.innerHTML = ` <h2> Total : \$${this.totalAmmount.toFixed(
+      2
+    )}</h2>`;
   }
 
   get totalAmmount() {
@@ -21,27 +60,44 @@ class ShoppingCart {
     );
     return sum;
   }
+
+  // as long as the subClass don't have constructor will called the inherit class,and if exist hen will called only the subclass construct.
+  // So need to use Super() to call both;
+  constructor(renderHookId) {
+    super(renderHookId);
+  }
   addProduct(product) {
     // this.items.push(product);
     const updatedItems = [...this.items];
     updatedItems.push(product);
     this.cartItems = updatedItems;
   }
+  orderProduts(){
+
+
+
+    console.log(' Order is coming .............');
+     console.log(this.items)
+  }
 
   render() {
-    const cartEl = document.createElement("section");
+    // const cartEl = document.createElement("section");
+    const cartEl = this.createRootElement("section", "cart");
     cartEl.innerHTML = `
     <h2> Total : \$${0}</h2>
     <button>Order Now!</button> `;
-    cartEl.className = "cart";
+    // cartEl.className = "cart";
+    const orderButton = cartEl.querySelector('button');
+    orderButton.addEventListener('click',() => this.orderProduts())
     this.totalOutput = cartEl.querySelector("h2");
-    return cartEl;
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
   addToCart() {
     // console.log(" Adding product to cart ...");
@@ -49,8 +105,9 @@ class ProductItem {
     App.addProductToCart(this.product);
   }
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement("li", "product-item");
+    // const prodEl = document.createElement("li");
+    // prodEl.className = "product-item";
     prodEl.innerHTML = `
         <div>
         <img  src ="${this.product.imageUrl}" alt =" ${this.product.title}">
@@ -64,65 +121,97 @@ class ProductItem {
         `;
     const addCartButton = prodEl.querySelector("button");
     addCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodEl;
+    // return prodEl;
   }
 }
-class ProductList {
-  products = [
-    new Product(
-      "Nikon Z89",
-      "https://in.canon/media/image/2018/05/03/642e7bbeae5741e3b872e082626c0151_eos6d-mkii-ef-24-70m-l.png",
-      99.99,
-      "High qulaity Canon camera"
-    ),
+class ProductList extends Component {
+  products = [];
+  constructor(renderHookId) {
+    super(renderHookId);
+    this.fetchProducts();
+  }
+  fetchProducts() {
+    this.products = [
+      new Product(
+        "Nikon Z89",
+        "https://in.canon/media/image/2018/05/03/642e7bbeae5741e3b872e082626c0151_eos6d-mkii-ef-24-70m-l.png",
+        99.99,
+        "High qulaity Canon camera"
+      ),
 
-    new Product(
-      "Camera Lens",
-      "https://www.businessinsider.in/thumb/msid-77720373,width-960,resizemode-4,imgsize-58282/tech-buying-guides/best-budget-dslr-cameras-in-india/best-budget-dslr-cameras.jpg",
-      49.99,
-      " Far quality zoom camera "
-    ),
-    new Product(
-      "Camera stand",
-      "https://i.ytimg.com/vi/ei1_z7XlUz0/maxresdefault.jpg",
-      59.99,
-      " High Quality camera stand "
-    ),
-  ];
+      new Product(
+        "Camera Lens",
+        "https://www.businessinsider.in/thumb/msid-77720373,width-960,resizemode-4,imgsize-58282/tech-buying-guides/best-budget-dslr-cameras-in-india/best-budget-dslr-cameras.jpg",
+        49.99,
+        " Far quality zoom camera "
+      ),
+      new Product(
+        "Camera stand",
+        "https://i.ytimg.com/vi/ei1_z7XlUz0/maxresdefault.jpg",
+        59.99,
+        " High Quality camera stand "
+      ),
+    ];
+    this.renderProduct();
+  }
 
+  renderProduct() {
+    for (const prod of this.products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
   render() {
     // const renderHook = document.getElementById("app");
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
-    for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
 
-      prodList.append(prodEl);
+    this.createRootElement("ul", "product-list", [
+      new ElementAttributes("id", "prod-list"),
+    ]);
+    if (this.product && this.product.length > 0) {
+      this.renderProduct();
     }
-    return prodList;
+    // prodList.id = 'prod-list';
+    // prodList.className = "product-list";
+
+    // const prodList = this.createRootElement('ul',)
+
+    // for (const prod of this.products) {
+    //   new ProductItem(prod, "prod-list");
+    //   // const productItem = new ProductItem(prod, "prod-list");
+    //   // const prodEl = productItem.render();
+    //   // productItem.render();
+
+    //   // prodList.append(prodEl);
+    // }
+    // return prodList;
     // renderHook.append(prodList);
   }
 }
 
 class Market {
+  constructor() {
+    // Can done too by extends to the Compnents class and execute super(), but no need
+    this.render();
+  }
   render() {
-    const renderHook = document.getElementById("app");
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
+    // const renderHook = document.getElementById("app");
+    // app initilazed as id to shopping cart contructor to trnsform to the inherit class
+    this.cart = new ShoppingCart("app");
+    // this.cart.render();
+    // const productList = new ProductList("app");
+    new ProductList("app");
+    // const prodListEl = productList.render();
+    // productList.render();
 
-    renderHook.append(cartEl);
-    renderHook.append(prodListEl);
+    // renderHook.append(prodListEl);
   }
 }
 
 class App {
   static cart;
   static init() {
-    const market = new Market();
-    market.render();
+     const market = new Market();
+    // new Market();
+    // market.render();
     this.cart = market.cart;
   }
   static addProductToCart(product) {
